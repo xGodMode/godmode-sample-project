6d58251791f2340994ca2423a0ab71f43c696627
benjamistan <ben@benstanley.pro>
2021-06-08 12:58:45 +0200

    fix: typos

--- README.md
+++ README.md
@@ -1,32 +1,38 @@
-
 ![GodMode Logo](https://godmode-public-assets.s3.amazonaws.com/godmode_logo.jpg)
 
 This sample project demonstrates how GodMode allows you to gain control of contracts running on an instance of Ganache CLI.
 
 With `CustomContracts.test.js`, Alex has a contract with a privileged action (a Boolean flag that can be flipped only by him). Using GodMode, Beth can access this privileged action and flip the flag, too.
 
+With `ExternalContracts.test.js`, we can perform actions on Maker, Uniswap and Compound that we don't have priveleges for on Mainnet.
+
 ### Pre-Requisites
+
 ---
-1. Install the [Truffle framework](https://github.com/trufflesuite/truffle) via `$ npm install -g truffle`.
 
-2. Clone the [GodMode Ganache CLI](https://github.com/xGodMode/godmode-ganache-cli) repository.
+1. Install the Truffle framework via `$ npm install -g truffle`.
 
+2. Install and run an instance of the [GodMode Ganache CLI](https://github.com/xGodMode/godmode-ganache-cli).
+
+3. An [Infura](https:/infura.io) **Mainnet** endpoint (for demonstrating the external contracts running on a copy of mainnet).
 
 ### Custom Contracts
+
 ---
 
 1. Open a shell/cmd in your **godmode-ganache-cli** directory and run `$ npm run start`.
 
-2. In another shell/cmd, clone this repository into **godmode-sample-projects** , cd into this new directory and run `$ npm install` 
+2. In another shell/cmd, clone this repository into **godmode-sample-projects** , cd into this new directory and run `$ npm install`
 
-3. By default, **GodMode Ganache CLI** creates an endpoint at `http://127.0.0.1:8545`. In `CustomContracts.test.js` you can customise this by editing the line below. 
+3. By default, **GodMode Ganache CLI** creates an endpoint at `http://127.0.0.1:8545`. In `CustomContracts.test.js` you can customise this by editing the line below.
 
    ```js
    const GODMODE = new GM('development', 'http://127.0.0.1:8545');
    ```
 
+   By default, GodMode Ganache CLI runs at `http://127.0.0.1:8545`, so use this endpoint unless you're running a custom configuration.
 
-4. Finally, `cd` into the `test` folder and run `$ truffle test CustomContracts.test.js`
+4. Finally, at the root of your **godmode-sample-project** directory, run `$ truffle test test/CustomContracts.test.js`
 
 GodMode runs the tests showing before and after taking control of the a contract. Your test output should look like the following:
 
@@ -34,11 +40,47 @@ GodMode runs the tests showing before and after taking control of the a contract
           ✓ starts with Alex as the owner
           ✓ allows Alex to access the priviledgedAction (e.g. flip a boolean) (95ms)
           ✓ does not allow Beth to access the priviledgedAction (454ms)
-    
+
     HasOwnerShip in GODMODE
           ✓ GODMODE: allows Beth access to the privilegedAction! (148ms)
           ✓ GODMODE: allows Beth to become the owner! (127ms)
 
 ### External Contracts
+
 ---
-This section coming soon...
+
+1. At a command line in your **godmode-ganache-cli** folder, run `$ export INFURA_PROJECT_ID=<your-infura-project-id>`
+
+2. Run `$ npm run start-fork`.
+
+3. In another shell/cmd, clone this repository into **godmode-sample-projects** , cd into this new directory and run `$ npm install`
+
+4. Install the GodMode versions of the external contracts via `$ npx godmode install`
+
+5. By default, **GodMode Ganache CLI** creates an endpoint at `http://127.0.0.1:8545`. In `ExternalContracts.test.js` you can customise this by editing the line below.
+
+   ```js
+   const GODMODE = new GM('ethereum:mainnet', 'ws://127.0.0.1:8545');
+   ```
+
+   By default, GodMode Ganache CLI runs at `http://127.0.0.1:8545`, so use this endpoint unless you're running a custom configuration.
+
+6. Finally, at the root of your **godmode-sample-project** directory, run `$ truffle test test/ExternalContracts.test.js`
+
+Your output should look like the following:
+
+    Mainnet DAI
+    Done adding preset protocols.
+    Alex's starting balance: 0
+    Alex's ending balance: 200
+          ✓ GODMODE: can mint DAI to any account as much as I want (774ms)
+
+    Mainnet Uniswap V2
+    Starting fee collector is no one: 0x0000000000000000000000000000000000000000
+    Ending fee collector is Beth: 0x5F001a03679b1704DEee8d25976510fCA32550d4
+          ✓ GODMODE: can set any account I want as the fee collector (626ms)
+
+    Mainnet Compound
+    Carl's starting balance: 0
+    Carl's ending balance: 100
+          ✓ GODMODE: can give CTokens to any account I want (676ms)
--- test/ExternalContracts.test.js
+++ test/ExternalContracts.test.js
@@ -5,95 +5,108 @@
  *
  */
 
-const { GM } = require("@xgm/godmode");
-const UniswapV2FactoryContract = artifacts.require("UniswapV2FactoryContract");
-const CErc20Contract = artifacts.require("CErc20Contract");
-const Dai = artifacts.require("Dai");
+const { GM } = require('@xgm/godmode');
+const UniswapV2FactoryContract = artifacts.require('UniswapV2FactoryContract');
+const CErc20Contract = artifacts.require('CErc20Contract');
+const Dai = artifacts.require('Dai');
 
-const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 // IMPORTANT: You must run GodMode Ganache with a mainnet fork for this to work
-const GODMODE = new GM("ethereum:mainnet", "ws://127.0.0.1:8545");
+const GODMODE = new GM('ethereum:mainnet', 'ws://127.0.0.1:8545');
 
 function dimlog(msg) {
-  console.log("\x1b[2m%s\x1b[0m", msg);
+	console.log('\x1b[2m%s\x1b[0m', msg);
 }
 
-contract("External Contracts", function(accounts) {
-  const Alex = accounts[1];
-  const Beth = accounts[2];
-  const Carl = accounts[3];
+contract('External Contracts', function (accounts) {
+	const Alex = accounts[1];
+	const Beth = accounts[2];
+	const Carl = accounts[3];
 
-  describe("\nMainnet DAI", function(){
-    before(async function() { await GODMODE.open(); });
-    after(async function() { await GODMODE.close(); });
+	describe('\nMainnet DAI', function () {
+		before(async function () {
+			await GODMODE.open();
+		});
+		after(async function () {
+			await GODMODE.close();
+		});
 
-    it("GODEMODE: can mint DAI to any account as much as I want", async function(){
-      let mainnetDaiAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
-      let daiContract = await Dai.at(mainnetDaiAddress);
+		it('GODMODE: can mint DAI to any account as much as I want', async function () {
+			let mainnetDaiAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
+			let daiContract = await Dai.at(mainnetDaiAddress);
 
-      // Balance starts at 0
-      let startingBalance = await daiContract.balanceOf(Alex);
-      assert.equal(startingBalance, 0);
-      dimlog(`Alex's starting balance: ${startingBalance.toString()}`);
+			// Balance starts at 0
+			let startingBalance = await daiContract.balanceOf(Alex);
+			assert.equal(startingBalance, 0);
+			dimlog(`Alex's starting balance: ${startingBalance.toString()}`);
 
-      // Mint x times
-      let x = Math.floor(Math.random() * 10) + 1;
-      for(var i = 0 ; i < x; i++){
-        await GODMODE.Maker.mintDai(Alex, 100);
-      }
+			// Mint x times
+			let x = Math.floor(Math.random() * 10) + 1;
+			for (var i = 0; i < x; i++) {
+				await GODMODE.Maker.mintDai(Alex, 100);
+			}
 
-      // Balance ends at 100x!
-      let endingBalance = await daiContract.balanceOf(Alex);
-      assert.equal(endingBalance, 100 * x);
-      dimlog(`Alex's ending balance: ${endingBalance.toString()}`);
-    });
-  });
+			// Balance ends at 100x!
+			let endingBalance = await daiContract.balanceOf(Alex);
+			assert.equal(endingBalance, 100 * x);
+			dimlog(`Alex's ending balance: ${endingBalance.toString()}`);
+		});
+	});
 
-  describe("\nMainnet Uniswap V2", function(){
-    before(async function() { await GODMODE.open(); });
-    after(async function() { await GODMODE.close(); });
+	describe('\nMainnet Uniswap V2', function () {
+		before(async function () {
+			await GODMODE.open();
+		});
+		after(async function () {
+			await GODMODE.close();
+		});
 
-    it("GODMODE: can set any account I want as the fee collector", async function(){
-      let mainnetFactoryAddress = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
-      let factoryContract = await UniswapV2FactoryContract.at(mainnetFactoryAddress);
+		it('GODMODE: can set any account I want as the fee collector', async function () {
+			let mainnetFactoryAddress = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f';
+			let factoryContract = await UniswapV2FactoryContract.at(
+				mainnetFactoryAddress
+			);
 
-      // Initially, feeTo is not set, and no fee is collected
-      let startingFeeCollector = await factoryContract.feeTo();
-      assert.equal(startingFeeCollector, ZERO_ADDRESS);
-      dimlog(`Starting fee collector is no one: ${startingFeeCollector}`);
+			// Initially, feeTo is not set, and no fee is collected
+			let startingFeeCollector = await factoryContract.feeTo();
+			assert.equal(startingFeeCollector, ZERO_ADDRESS);
+			dimlog(`Starting fee collector is no one: ${startingFeeCollector}`);
 
-      // Set feeTo
-      await GODMODE.UniswapV2.Factory_setFeeTo(Beth);
+			// Set feeTo
+			await GODMODE.UniswapV2.Factory_setFeeTo(Beth);
 
-      // feeTo is set!
-      let endingFeeCollector = await factoryContract.feeTo();
-      assert.equal(endingFeeCollector, Beth);
-      dimlog(`Ending fee collector is Beth: ${endingFeeCollector}`);
-    });
-  });
+			// feeTo is set!
+			let endingFeeCollector = await factoryContract.feeTo();
+			assert.equal(endingFeeCollector, Beth);
+			dimlog(`Ending fee collector is Beth: ${endingFeeCollector}`);
+		});
+	});
 
-  describe("\nMainnet Compound", function(){
-    before(async function() { await GODMODE.open(); });
-    after(async function() { await GODMODE.close(); });
+	describe('\nMainnet Compound', function () {
+		before(async function () {
+			await GODMODE.open();
+		});
+		after(async function () {
+			await GODMODE.close();
+		});
 
-    it("GODMODE: can give CTokens to any account I want", async function(){
-      let mainnetCBAT = "0x6c8c6b02e7b2be14d4fa6022dfd6d75921d90e4e";
-      let cBAT = await CErc20Contract.at(mainnetCBAT);
+		it('GODMODE: can give CTokens to any account I want', async function () {
+			let mainnetCBAT = '0x6c8c6b02e7b2be14d4fa6022dfd6d75921d90e4e';
+			let cBAT = await CErc20Contract.at(mainnetCBAT);
 
-      // Balance starts at 0
-      let startingBalance = await cBAT.balanceOf(Carl);
-      assert.equal(startingBalance, 0);
-      dimlog(`Carl's starting balance: ${startingBalance.toString()}`);
+			// Balance starts at 0
+			let startingBalance = await cBAT.balanceOf(Carl);
+			assert.equal(startingBalance, 0);
+			dimlog(`Carl's starting balance: ${startingBalance.toString()}`);
 
-      // Give address tokens
-      await GODMODE.Compound.mintCErc20(mainnetCBAT, Carl, 100);
+			// Give address tokens
+			await GODMODE.Compound.mintCErc20(mainnetCBAT, Carl, 100);
 
-      // Balance ends at 100!
-      let endingBalance = await cBAT.balanceOf(Carl);
-      assert.equal(endingBalance, 100);
-      dimlog(`Carl's ending balance: ${endingBalance.toString()}`);
-    })
-  });
+			// Balance ends at 100!
+			let endingBalance = await cBAT.balanceOf(Carl);
+			assert.equal(endingBalance, 100);
+			dimlog(`Carl's ending balance: ${endingBalance.toString()}`);
+		});
+	});
 });
-
